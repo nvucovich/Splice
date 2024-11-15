c92df5d7d6d56732083ef498e51238ec72d2c0e8
Nathan Vucovich <nvucovich@gmail.com>
2024-11-15 18:18:29 -0500

    Initial Commit

---
+++ Splice/SpliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpliceApp.swift
+//  Splice
+//
+//  Created by NVucovich on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SpliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpliceTests/SpliceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpliceTests.swift
+//  SpliceTests
+//
+//  Created by NVucovich on 11/15/24.
+//
+
+import Testing
+@testable import Splice
+
+struct SpliceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
